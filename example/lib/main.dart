ce81553cf2fd6697a3a861b6b8a417e5d211b9f6
Alvaro Prado <alvarob.prado@hotmail.com>
2023-01-07 14:09:58 -0400

    feat: create counter example with ReStateAction

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:re_state_action/re_state_action.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,12 +28,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final ReCounterStateAction _reCounterStateAction = ReCounterStateAction();
 
   void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+    _reCounterStateAction.increment();
+  }
+
+  void _onCounterAction(ReCounterAction action) {
+    ScaffoldMessenger.of(context)
+      ..removeCurrentSnackBar()
+      ..showSnackBar(
+        SnackBar(
+          content: Text('Snackbar Color: ${action.colorName}'),
+          backgroundColor: action.color,
+        ),
+      );
+  }
+
+  void _onResetCounter() {
+    _reCounterStateAction.reset();
   }
 
   @override
@@ -40,6 +54,12 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: _onResetCounter,
+          ),
+        ],
       ),
       body: Center(
         child: Column(
@@ -48,9 +68,13 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            ReStateActionWidget(
+              reState: _reCounterStateAction,
+              onAction: _onCounterAction,
+              builder: (context, state, child) => Text(
+                '$state',
+                style: Theme.of(context).textTheme.headline4,
+              ),
             ),
           ],
         ),
@@ -62,4 +86,50 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+}
+
+class ReCounterStateAction extends ReStateAction<int, ReCounterAction> {
+  ReCounterStateAction() : super(0) {
+    listenState(
+      (state) {
+        if (state == 0) {
+          return;
+        } else if (state % 3 == 0) {
+          emitAction(const ShowSnackRed());
+        } else if (state % 2 == 0) {
+          emitAction(const ShowSnackGreen());
+        }
+      },
+    );
+  }
+
+  void increment() {
+    emitState(state + 1);
+  }
+
+  void reset() {
+    emitState(0);
+    emitAction(const ShowSnackBrown());
+  }
+}
+
+abstract class ReCounterAction {
+  const ReCounterAction(
+    this.color,
+    this.colorName,
+  );
+  final Color color;
+  final String colorName;
+}
+
+class ShowSnackRed extends ReCounterAction {
+  const ShowSnackRed() : super(Colors.red, 'Red');
+}
+
+class ShowSnackGreen extends ReCounterAction {
+  const ShowSnackGreen() : super(Colors.green, 'Green');
+}
+
+class ShowSnackBrown extends ReCounterAction {
+  const ShowSnackBrown() : super(Colors.brown, 'Brown - Reseting');
 }
