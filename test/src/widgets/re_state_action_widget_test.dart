9560ef4fbee21584eb83d38ef03584b249738cb0
Álvaro Prado <alvaroprado@tokenlab.com.br>
2023-03-08 07:47:55 -0400

    test: add scenarios for ReStateActionWidget

--- test/src/widgets/re_state_action_widget_test.dart
+++ test/src/widgets/re_state_action_widget_test.dart
@@ -14,48 +14,146 @@ void main() {
 
   setUp(() {
     reState = TestStateAction(0);
+    actionText = 'Test';
   });
 
-  testWidgets(
-    'Rebuilds when state changes',
-    (tester) async {
-      final widget = MaterialApp(
-        home: ReStateActionWidget<int, String>(
-          reState: reState,
-          onAction: onAction,
-          builder: (context, state, child) => Text('$state'),
-        ),
+  group(
+    'Builder',
+    () {
+      testWidgets(
+        'rebuilds when state changes',
+        (tester) async {
+          final widget = MaterialApp(
+            home: ReStateActionWidget<int, String>(
+              reState: reState,
+              onAction: onAction,
+              builder: (context, state, child) => Text('$state'),
+            ),
+          );
+
+          await tester.pumpWidget(widget);
+          await tester.pumpAndSettle();
+          expect(find.text('0'), findsOneWidget);
+          expect(find.text('1'), findsNothing);
+          reState.increment();
+          await tester.pumpAndSettle();
+          expect(find.text('1'), findsOneWidget);
+          expect(find.text('0'), findsNothing);
+          expect(actionText, 'Test');
+        },
       );
 
-      await tester.pumpWidget(widget);
-      await tester.pumpAndSettle();
-      expect(find.text('0'), findsOneWidget);
-      expect(find.text('1'), findsNothing);
-      reState.increment();
-      await tester.pumpAndSettle();
-      expect(find.text('1'), findsOneWidget);
-      expect(find.text('0'), findsNothing);
-      expect(actionText, 'Test');
+      testWidgets(
+        'not rebuild if buildWhen condition is false',
+        (widgetTester) async {
+          final widget = MaterialApp(
+            home: ReStateActionWidget<int, String>(
+              reState: reState,
+              onAction: onAction,
+              buildWhen: (previous, current) => current > 1,
+              builder: (context, state, child) => Text('$state'),
+            ),
+          );
+
+          await widgetTester.pumpWidget(widget);
+          await widgetTester.pumpAndSettle();
+          expect(find.text('0'), findsOneWidget);
+          expect(find.text('1'), findsNothing);
+          reState.increment();
+          await widgetTester.pumpAndSettle();
+          expect(find.text('1'), findsNothing);
+          expect(find.text('0'), findsOneWidget);
+          reState.increment();
+          await widgetTester.pumpAndSettle();
+          expect(find.text('0'), findsNothing);
+          expect(find.text('1'), findsNothing);
+          expect(find.text('2'), findsOneWidget);
+        },
+      );
+
+      testWidgets(
+        'not rebuild if buildWhen condition is false and action is emitted',
+        (widgetTester) async {
+          final widget = MaterialApp(
+            home: ReStateActionWidget<int, String>(
+              reState: reState,
+              onAction: onAction,
+              buildWhen: (previous, current) => current > 1,
+              builder: (context, state, child) => Text('$state'),
+            ),
+          );
+
+          await widgetTester.pumpWidget(widget);
+          await widgetTester.pumpAndSettle();
+          reState.dispatchAction();
+          await widgetTester.pumpAndSettle();
+          expect(find.text('0'), findsOneWidget);
+          expect(find.text('1'), findsNothing);
+          reState.increment();
+          await widgetTester.pumpAndSettle();
+          expect(find.text('1'), findsNothing);
+          expect(find.text('0'), findsOneWidget);
+          reState.dispatchAction();
+          await widgetTester.pumpAndSettle();
+          expect(find.text('0'), findsOneWidget);
+          expect(find.text('1'), findsNothing);
+          reState.increment();
+          await widgetTester.pumpAndSettle();
+          expect(find.text('0'), findsNothing);
+          expect(find.text('1'), findsNothing);
+          expect(find.text('2'), findsOneWidget);
+        },
+      );
     },
   );
 
-  testWidgets(
-    'Calls onAction when action is emitted',
-    (tester) async {
-      final widget = MaterialApp(
-        home: ReStateActionWidget<int, String>(
-          reState: reState,
-          onAction: onAction,
-          builder: (context, state, child) => Text('$state'),
-        ),
+  group(
+    'Action Listener',
+    () {
+      testWidgets(
+        'call onAction when action is emitted',
+        (tester) async {
+          final widget = MaterialApp(
+            home: ReStateActionWidget<int, String>(
+              reState: reState,
+              onAction: onAction,
+              builder: (context, state, child) => Text('$state'),
+            ),
+          );
+
+          await tester.pumpWidget(widget);
+          await tester.pumpAndSettle();
+          reState.dispatchAction();
+          await tester.pumpAndSettle();
+          expect(actionText, 'Test action');
+          expect(find.text('0'), findsOneWidget);
+        },
       );
 
-      await tester.pumpWidget(widget);
-      await tester.pumpAndSettle();
-      reState.dispatchAction();
-      await tester.pumpAndSettle();
-      expect(actionText, 'Test action');
-      expect(find.text('0'), findsOneWidget);
+      testWidgets(
+        'not call onAction if listenWhen condition is false',
+        (widgetTester) async {
+          final widget = MaterialApp(
+            home: ReStateActionWidget<int, String>(
+              reState: reState,
+              onAction: onAction,
+              listenWhen: (previous, current) => previous != null,
+              builder: (context, state, child) => Text('$state'),
+            ),
+          );
+
+          await widgetTester.pumpWidget(widget);
+          await widgetTester.pumpAndSettle();
+          reState.dispatchAction();
+          await widgetTester.pumpAndSettle();
+          expect(actionText, 'Test');
+          expect(find.text('0'), findsOneWidget);
+          reState.dispatchAction();
+          await widgetTester.pumpAndSettle();
+          expect(actionText, 'Test action');
+          expect(find.text('0'), findsOneWidget);
+        },
+      );
     },
   );
 }
